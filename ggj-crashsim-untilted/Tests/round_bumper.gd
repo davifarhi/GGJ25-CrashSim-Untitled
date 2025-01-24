586350bc3d8522e4f4b1fc485ddd4d1dad69f1dc
Davi Farhi <davifarhi@gmail.com>
2025-01-25 00:00:14 +0100

    bumper tests

---
+++ ggj-crashsim-untilted/Tests/round_bumper.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+@onready var sprite = $AnimationPlayer
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	sprite.play("Bump")
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
