8541b72494a6f283bc0f96fb90492dc78a0fe64e
ESoria <soria.etienne@gmail.com>
2025-01-24 23:37:13 +0100

    Add some damp

--- ggj-crashsim-untilted/scenes/bubbleMovement.gd
+++ ggj-crashsim-untilted/scenes/bubbleMovement.gd
@@ -11,9 +11,8 @@ func doInpulse() -> void:
 func _ready() -> void:
 	pass
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
+func _process(_delta: float)  -> void:
 	if (Input.is_action_just_pressed("Inpulse")):
 		doInpulse()
 		print("Flash")
