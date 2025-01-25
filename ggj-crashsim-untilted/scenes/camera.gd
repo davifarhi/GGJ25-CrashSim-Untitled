3c73d6266eca8e2dd5cf5ba5b1f046653f1cea72
ESoria <soria.etienne@gmail.com>
2025-01-25 22:28:59 +0100

    Add lockCam

--- ggj-crashsim-untilted/scenes/camera.gd
+++ ggj-crashsim-untilted/scenes/camera.gd
@@ -8,7 +8,6 @@ extends Camera2D
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var d = boubou.position.x - position.x
