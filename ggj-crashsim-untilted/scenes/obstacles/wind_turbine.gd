c72be3b67362b12a72ab888dce1ffe709826d2ca
Xavier Niochaut <jglrxavpok@gmail.com>
2025-01-25 18:44:12 +0100

    Wind particles

--- ggj-crashsim-untilted/scenes/obstacles/wind_turbine.gd
+++ ggj-crashsim-untilted/scenes/obstacles/wind_turbine.gd
@@ -9,9 +9,15 @@ func _ready() -> void:
 	$WindBox.acceleration_amount = acceleration_amount
 	$WindBox.pull = pull
 	
-	# TODO: particles
-	#windbox_shape.hide()
+	windbox_shape.hide()
+	var size = windbox_shape.global_scale
+	$WindBox/WindBoxShape/Particles.amount *= size.x*size.y
 	$WindBox/WindBoxShape.global_transform = windbox_shape.global_transform
 
 func _process(_delta: float) -> void:
+	var topLeftCorner = windbox_shape.global_position
+	var size = windbox_shape.global_scale
+	var particleMat = $WindBox/WindBoxShape/Particles.process_material as ParticleProcessMaterial
+	particleMat.emission_box_extents = Vector3(size.x*32, size.y*8, 0)
+
 	pass
