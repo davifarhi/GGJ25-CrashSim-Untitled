7ebf2e1cff0d1bd88c7de7eb8be747994c8294d9
ESoria <soria.etienne@gmail.com>
2025-01-25 01:11:58 +0100

    Add inpulse indicator

--- ggj-crashsim-untilted/scenes/bubbleMovement.gd
+++ ggj-crashsim-untilted/scenes/bubbleMovement.gd
@@ -2,17 +2,25 @@ extends RigidBody2D
 
 @export var InpulseForce:float = 10
 
+@export_category("Indicator")
+@export var Indicator:Node2D
+@export var IndicatorDistance:float = 32;
+
 var impulsionDone = false
-var isInputInControllerMode = true
 
 func doInpulse() -> void:
-	apply_central_impulse(InputDir() * InpulseForce);
+	apply_central_impulse(- InputDir() * InpulseForce);
 	
 func InputDir() -> Vector2:
-	if (isInputInControllerMode):
-		return Input.get_vector("JoypadDirRight","JoypadDirLeft", "JoypadDirDown", "JoypadDirUp")
-	else:
-		return Vector2()
+	var dir = Input.get_vector("JoypadDirLeft", "JoypadDirRight", "JoypadDirUp", "JoypadDirDown")
+	
+	if dir.length_squared() == 0:
+		dir = Vector2(-1, 0) # set a default value to avoid 0 div
+	
+	return dir.normalized()
+
+func UpdateIndicatorPos():
+	Indicator.position = InputDir() * IndicatorDistance
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -20,5 +28,6 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float)  -> void:
+	UpdateIndicatorPos()
 	if (Input.is_action_just_pressed("Inpulse")):
 		doInpulse()
