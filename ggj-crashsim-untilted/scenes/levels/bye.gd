c582b285901fc9de80560c5c25565a0a387bd7cf
Davi Farhi <davifarhi@gmail.com>
2025-01-26 16:27:10 +0100

    Added end screen and completion time

--- ggj-crashsim-untilted/scenes/levels/bye.gd
+++ ggj-crashsim-untilted/scenes/levels/bye.gd
@@ -3,7 +3,18 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print(GameManager.get_completion_time())
+	var time = GameManager.get_completion_time()
+	var rounded = floor(time)
+	var secs = int(rounded)
+	var msecs = int((time - rounded)*1000)
+	
+	var str = TimerText.format_str % [secs, msecs]
+	var text = $TimerText
+	
+	text.clear()
+	text.push_context()
+	text.push_color(TimerText.NORMAL_COLOR)
+	text.append_text(str)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -11,5 +22,37 @@ func _process(delta: float) -> void:
 	pass
 
 
-func _on_main_menu_pressed() -> void:
+@onready var buttons = $ByeButtons
+var mouse_navigation = true
+
+func release_all_focus():
+	if buttons == null: return
+	
+	for butt in buttons.get_children():
+		if butt != null:
+			butt.release_focus()
+		
+
+func select_first_button() -> bool:
+	if buttons == null or buttons.get_child_count() == 0:
+		return false
+	
+	if buttons.get_child(0) != null:
+		buttons.get_child(0).grab_focus()
+		return true
+	return false
+	
+	
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		if not mouse_navigation:
+			release_all_focus()
+			mouse_navigation = true
+	if event is InputEventJoypadButton or event is InputEventJoypadMotion:
+		if mouse_navigation:
+			release_all_focus()
+			mouse_navigation = !select_first_button()
+	
+
+func _on_texture_button_button_click() -> void:
 	GameManager.GoToMainMenu.emit()
