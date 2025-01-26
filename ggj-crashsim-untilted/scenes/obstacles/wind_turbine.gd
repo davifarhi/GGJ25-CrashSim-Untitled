b3f2582634af232bd169685726a99868548b981f
ESoria <soria.etienne@gmail.com>
2025-01-26 13:20:59 +0100

    Change windTurbine

--- ggj-crashsim-untilted/scenes/obstacles/wind_turbine.gd
+++ ggj-crashsim-untilted/scenes/obstacles/wind_turbine.gd
@@ -1,26 +1,44 @@
-extends Node2D
+extends Area2D
 
 @export var acceleration_amount: float = 1.0
 @export var pull: bool = false
-@export var windbox_shape: Sprite2D = null
+var shape:CollisionShape2D = null;
+var player:Boubou = null
+var currentEnvSound: AudioStreamPlayer = null
+@onready var contact_sfx_collection = SoundCollection.new($ContactSFX)
+@onready var contact_vo_collection = SoundCollection.new($ContactVO)
+@onready var animPlayer: AnimationPlayer = $StaticPart/AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$WindBox.acceleration_amount = acceleration_amount
-	$WindBox.pull = pull
-	
-	windbox_shape.hide()
-	var size = windbox_shape.global_scale
-	var particleMat = ($WindBox/WindBoxShape/Particles.process_material.duplicate() as ParticleProcessMaterial)
-	$WindBox/WindBoxShape/Particles.process_material = particleMat
-	particleMat.gravity = Vector3(0, 98, 0).rotated(Vector3(0,0,1), global_rotation)
-	$WindBox/WindBoxShape/Particles.amount *= size.x*size.y
-	$WindBox/WindBoxShape.global_transform = windbox_shape.global_transform
+	animPlayer.play("ventilateur_animation")
+	for child in get_children():
+		if child is CollisionShape2D:
+			shape = child as CollisionShape2D
+			
+	var ratio:float = shape.shape.get_rect().size.y / 256
+	$particlesOffset.scale.y = ratio
 
 func _process(_delta: float) -> void:
-	var topLeftCorner = windbox_shape.global_position
-	var size = windbox_shape.global_scale
-	var particleMat = $WindBox/WindBoxShape/Particles.process_material as ParticleProcessMaterial
-	particleMat.emission_box_extents = Vector3(size.x*32, size.y*8, 0)
-
 	pass
+
+func _physics_process(_delta: float) -> void:
+	if player:
+		var direction_factor: float = -1 if pull else 1;
+		var direction = Vector2(0, 1).rotated(global_rotation)
+		player.apply_central_force(direction_factor * acceleration_amount * direction * 1000)
+
+func _on_body_entered(body: Node2D) -> void:
+	if body is Boubou:
+		player = body as Boubou
+		if currentEnvSound == null or !currentEnvSound.playing:
+			currentEnvSound = contact_sfx_collection.select_random()
+			currentEnvSound.play()
+		contact_vo_collection.play_random()
+
+
+func _on_body_exited(body: Node2D) -> void:
+	if body is Boubou:
+		player = null
+		if currentEnvSound != null:
+			currentEnvSound.stop()
