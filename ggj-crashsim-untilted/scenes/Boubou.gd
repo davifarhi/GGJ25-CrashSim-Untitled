69ff888310d4d5c845521671416d3b62f273d77b
Xavier Niochaut <jglrxavpok@gmail.com>
2025-01-26 10:06:19 +0100

    Wind turbine anim + fix mouse controls

--- ggj-crashsim-untilted/scenes/Boubou.gd
+++ ggj-crashsim-untilted/scenes/Boubou.gd
@@ -29,6 +29,7 @@ signal BoubouBumperContact
 @onready var boubou_inpulse_vo_sfx_collection = SoundCollection.new($InpulseVOSFX)
 
 @onready var dir_indicator = $Visual/DirIndicator
+@onready var camera = get_parent().get_node("Camera2D") as Camera2D
 
 var impulsionDone = false
 var dead = false
@@ -59,7 +60,10 @@ func _input(event: InputEvent) -> void:
 func InputDir() -> Vector2:
 	var dir = Vector2(0, 0)
 	if currentInputType == InputType.Mouse:
-		dir = get_viewport().get_mouse_position() - global_position
+		var screen_pos = global_position
+		if camera != null:
+			screen_pos = global_position - camera.global_position + Vector2(get_viewport().size)/2
+		dir = get_viewport().get_mouse_position() - screen_pos
 	elif currentInputType == InputType.Gamepad:
 		dir = Input.get_vector("JoypadDirLeft", "JoypadDirRight", "JoypadDirUp", "JoypadDirDown")
 	
